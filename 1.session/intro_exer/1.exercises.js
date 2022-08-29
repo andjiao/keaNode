0616e577e625ae66a73c9d7ee9f3aa5b7caaad37
andreajiao <andreajiao@gmail.com>
2022-08-29 10:34:05 +0200

    1.exercises solved

---
+++ 1.session/intro_exer/1.exercises.js
@@ -0,0 +1,31 @@
+// --------------------------------------
+// Variables, strings, numbers
+// --------------------------------------
+// Exercise 1 - Console and constiables
+
+const firstName = "Anders";
+const lastName = "Latif";
+
+
+// Print out the following in the console:
+// My first name is Anders and my last name is Latif
+
+console.log('my first name is ', firstName,'my last name is', lastName)
+
+
+// --------------------------------------
+// Exercise 2 - Numbers and Strings
+
+const year = "2021";
+const number = 1;
+
+// Add the year plus the number
+// The result should be 2022
+// You cannot touch line 1 or 2
+
+//bactic gør at man kan omforme variablen til en string
+//const r = = +year + number 
+// const r = Number(year) + number
+const message = `${year}`
+const together = parseInt(2021) + number
+console.log(together);
